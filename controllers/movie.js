a444b20cc85425f9c9e3ef67dfe20c6252333455
Dmitrii Sapegin <lejeboka24@gmail.com>
2022-12-06 15:50:37 +0300

    ...req.body

--- controllers/movie.js
+++ controllers/movie.js
@@ -14,8 +14,7 @@ module.exports.getMovies = async (req, res, next) => {
 
 module.exports.createMovie = async (req, res, next) => {
   try {
-    const { name, link } = req.body;
-    const movie = await Movie.create({ name, link, owner: req.user._id });
+    const movie = await Movie.create({ ...req.body, owner: req.user._id });
     res.send(movie);
   } catch (error) {
     if (error.name === 'ValidationError') {
